bf0c0ae1b918feff9cac800a55793c6026d52484
saeromCho <saerom.r.cho@gmail.com>
2020-02-18 22:23:26 +0900

    Set webpack config related in font-loader and add url-loader module

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,20 @@ module.exports = {
         ],
         exclude: /node_modules/
       },
+      {
+        // write files under 10k to inline or copy files over 10k
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
     ]
   },
 
