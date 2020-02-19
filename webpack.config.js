2efb06d5479c43b41d5c070b9b8deccd3298ad81
saeromCho <saerom.r.cho@gmail.com>
2020-02-19 16:24:01 +0900

    Reset .gitignore and webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -91,13 +91,13 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
     // mode: "development",
   },
   
   devServer: {
-    contentBase: path.join(__dirname, "dist"), // 이 경로에 있는 파일이 변경될 때 번들을 다시 컴파일
+    contentBase: path.join(__dirname, "build"), // 이 경로에 있는 파일이 변경될 때 번들을 다시 컴파일
     compress: true,
     port: 8080,//각자의 portNumber 작성
   },
