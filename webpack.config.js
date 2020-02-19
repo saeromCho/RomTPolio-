63314fbfff7218f4fa1351f24e300542c130251f
saeromCho <saerom.r.cho@gmail.com>
2020-02-19 17:19:42 +0900

    remove clean-webpack-plugin module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry : './src/index',
@@ -74,9 +73,6 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['build']
-    }),
     new HtmlWebpackPlugin({
       template: `./public/index.html`
     }),
